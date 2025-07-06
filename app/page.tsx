804021c600bf5810655623b22900f69f32ac4a3d
Ctorum <dev.rezendevitor@gmail.com>
2025-07-06 19:00:54 -0300

    refactor(app/page.tsx): refactored main app structure

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,8 @@ export default function Home() {
   return (
     <div className="min-h-screen w-full bg-background dark:bg-gray-900">
       <Header />
-
-      {/* Hero Section with Background */}
       <main>
-
-        <section className="h-screen w-full relative bg-[#D9D9D9] dark:bg-gray-900">
-        
-        </section>
-
-        {/* Countdown Section - Now below the hero section */}
+        <section className="h-screen w-full relative bg-[#D9D9D9] dark:bg-gray-900"/>
         <section className="w-full py-16 bg-white dark:bg-gray-900">
           <Image src={plant} alt="" className="absolute top-[100vh]"/>
           <div className="container mx-auto px-4 mt-[26.5rem]">
